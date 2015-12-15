7be8e45c4547a1853a5a3a4dc67db276d57753ab
Andrew Cole <andrew.cole3@hpe.com>
2015-12-14 17:05:35 -0800

    day 9

--- day09/1.rb
+++ day09/1.rb
@@ -1,32 +1,38 @@
 #!/usr/bin/env ruby
 
-input = File.readlines(ARGV[0]).collect{|l|l.chomp}
+input = File.readlines(ARGV[0]).collect{|line|line.chomp}
 all_cities = []
-arr = input.collect do |l|
-  cities, distance = l.split ' = '
+arr = input.collect do |line|
+  cities, distance = line.split ' = '
   l, r = cities.split ' to '
   all_cities << l
   all_cities << r
   [distance.to_i, l, r]
 end
+inverses = arr.collect do |line|
+  [line[0], line[2], line[1]]
+end
+arr += inverses
 
-arr.sort!{|a,b| a[0] <=> b[0]}
 all_cities.uniq!
 
-at = arr.first[1]
-visited = []
-distance = 0
-
-puts "Starting at #{at}"
-
-while visited.size < all_cities.size
-  candidates = arr.select{|distance, origin, destination| origin == at}
-  candidates.sort!{|a,b| a[0] <=> b[0]}
-  trip = candidates.first
-  at = trip.last
-  visited << at
-  distance += trip.first
+trips = []
+all_cities.permutation do |itin|
+  prev_city = nil
+  distance = itin.inject(0) do |sum, next_city|
+    retval = 0
+    if !prev_city.nil?
+      leg = arr.detect{|way| way[1]==prev_city && way[2]==next_city}
+      retval = sum + leg[0]
+    end
+    prev_city = next_city
+    retval
+  end
+  trips << ([distance] + itin)
 end
 
-puts visited.join ' -> '
-puts "Distance: #{distance}"
+trips.sort!{|a,b| a[0] <=> b[0]}
+
+trips.each do |t|
+  puts "Distance: #{t[0]}, #{t[1..-1].join ' -> '}"
+end
