ca96ef44f5c050cedb9338728ba3b5a158149cb8
Andrew Cole <aocole@gmail.com>
2015-12-18 21:27:06 -0800

    Day 18 pt 2

---
+++ day18/2.rb
@@ -0,0 +1,99 @@
+#!/usr/bin/env ruby
+
+ON = '#'
+OFF = '.'
+
+require "curses"
+include Curses
+
+def onsig(sig)
+  close_screen
+  exit sig
+end
+
+for i in 1 .. 15  # SIGHUP .. SIGTERM
+  begin
+    if trap(i, "SIG_IGN") != 0  # 0 for SIG_IGN
+      trap(i) {|sig| onsig(sig) }
+    end
+  rescue
+  end
+end
+
+init_screen
+nl
+noecho
+
+infile = ARGV[0]
+steps = ARGV[1].to_i
+@grid = File.readlines(infile).collect{|l|l.chomp.split('')}
+resizeterm(@grid.size, @grid.first.size)
+
+def set(line, col, val)
+  @grid[line][col] = val
+end
+
+def get(line, col)
+  @grid[line][col]
+end
+
+def on?(line, col)
+  get(line, col) == ON
+end
+
+def calc_next(line, col)
+  num_on = 0
+  ((line - 1)..(line + 1)).each do |l|
+    ((col - 1)..(col + 1)).each do |c|
+      if l >= 0 && l < @ldim
+        if c >= 0 && c < @cdim
+          next if l == line && c == col
+          num_on += 1 if on?(l, c)
+        end
+      end
+    end
+  end
+  if on?(line, col)
+    num_on == 2 || num_on == 3 ? ON : OFF
+  else
+    num_on == 3 ? ON : OFF
+  end
+end
+
+@ldim = @grid.size
+@cdim = @grid.first.size
+
+(steps+1).times do |i| 
+  # corners stuck on
+  @grid[0][0] = ON
+  @grid[0][-1] = ON
+  @grid[-1][0] = ON
+  @grid[-1][-1] = ON
+
+  next_grid = []
+  @ldim.times do |l|
+    next_grid[l] = []
+    @cdim.times do |c|
+      setpos(l, c); addstr(get(l, c))
+      next_grid[l][c] = calc_next(l, c)
+    end
+  end
+
+  refresh
+
+  if i == steps
+    on = 0
+    @grid.each do |l|
+      l.each do |light|
+        on += 1 if light == ON
+      end
+    end
+    setpos(@ldim+1, 0); puts "DONE: #{on} ON"
+    refresh
+    sleep 1000
+  end
+
+  @grid = next_grid
+  sleep 0.1
+end
+
